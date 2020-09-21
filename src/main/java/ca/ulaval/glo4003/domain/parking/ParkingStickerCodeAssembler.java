8de7885f4ecd0c4a2ab4d95ec241b012328ee4f7
Fabien Roy <fabienroy28@gmail.com>
2020-09-21 11:06:12 -0400

    [FEATURE] Add parking sticker code assembler test

--- src/main/java/ca/ulaval/glo4003/domain/parking/ParkingStickerCodeAssembler.java
+++ src/main/java/ca/ulaval/glo4003/domain/parking/ParkingStickerCodeAssembler.java
@@ -4,7 +4,6 @@ import ca.ulaval.glo4003.api.parking.dto.ParkingStickerCodeDto;
 
 public class ParkingStickerCodeAssembler {
   public ParkingStickerCodeDto assemble(ParkingStickerCode parkingStickerCode) {
-    // TODO : Test ParkingStickerCodeAssembler::assemble(ParkingStickerCode)
     ParkingStickerCodeDto parkingStickerCodeDto = new ParkingStickerCodeDto();
     parkingStickerCodeDto.code = parkingStickerCode.toString();
     return parkingStickerCodeDto;
