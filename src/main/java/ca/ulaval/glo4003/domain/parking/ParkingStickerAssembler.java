8a879418b0b065bfe984d3aeed6c276745fc7375
Fabien Roy <fabienroy28@gmail.com>
2020-09-21 11:02:02 -0400

    [FEATURE] Add picker assembler test

--- src/main/java/ca/ulaval/glo4003/domain/parking/ParkingStickerAssembler.java
+++ src/main/java/ca/ulaval/glo4003/domain/parking/ParkingStickerAssembler.java
@@ -2,12 +2,18 @@ package ca.ulaval.glo4003.domain.parking;
 
 import ca.ulaval.glo4003.api.parking.dto.ParkingStickerDto;
 import ca.ulaval.glo4003.domain.account.AccountId;
-import java.util.UUID;
+import ca.ulaval.glo4003.domain.account.AccountIdAssembler;
 
 public class ParkingStickerAssembler {
+  private final AccountIdAssembler accountIdAssembler;
+
+  public ParkingStickerAssembler(AccountIdAssembler accountIdAssembler) {
+    this.accountIdAssembler = accountIdAssembler;
+  }
+
   public ParkingSticker assemble(ParkingStickerDto parkingStickerDto) {
-    // TODO : ParkingStickerAssembler::assemble(ParkingStickerDto)
-    return new ParkingSticker(
-        new AccountId(UUID.randomUUID()), new ParkingAreaCode(parkingStickerDto.parkingArea));
+    AccountId accountId = accountIdAssembler.assemble(parkingStickerDto.accountId);
+
+    return new ParkingSticker(accountId, new ParkingAreaCode(parkingStickerDto.parkingArea));
   }
 }
---
+++ src/test/java/ca/ulaval/glo4003/domain/parking/ParkingStickerAssemblerTest.java
@@ -0,0 +1,53 @@
+package ca.ulaval.glo4003.domain.parking;
+
+import static ca.ulaval.glo4003.api.parking.helpers.ParkingStickerDtoBuilder.aParkingStickerDto;
+import static ca.ulaval.glo4003.domain.account.helpers.AccountObjectMother.createAccountId;
+import static ca.ulaval.glo4003.domain.parking.helpers.ParkingAreaObjectMother.createParkingAreaCode;
+
+import ca.ulaval.glo4003.api.parking.dto.ParkingStickerDto;
+import ca.ulaval.glo4003.domain.account.AccountId;
+import ca.ulaval.glo4003.domain.account.AccountIdAssembler;
+import com.google.common.truth.Truth;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ParkingStickerAssemblerTest {
+  private static final AccountId ACCOUNT_ID = createAccountId();
+  private static final String PARKING_AREA = createParkingAreaCode().toString();
+
+  @Mock private AccountIdAssembler accountIdAssembler;
+
+  private ParkingStickerDto parkingStickerDto;
+
+  private ParkingStickerAssembler parkingStickerAssembler;
+
+  @Before
+  public void setUp() {
+    parkingStickerAssembler = new ParkingStickerAssembler(accountIdAssembler);
+
+    BDDMockito.given(accountIdAssembler.assemble(ACCOUNT_ID.toString())).willReturn(ACCOUNT_ID);
+  }
+
+  @Test
+  public void givenAccountId_whenAssembling_thenReturnParkingStickerWithAccountId() {
+    parkingStickerDto = aParkingStickerDto().withAccountId(ACCOUNT_ID.toString()).build();
+
+    ParkingSticker parkingSticker = parkingStickerAssembler.assemble(parkingStickerDto);
+
+    Truth.assertThat(parkingSticker.getAccountId()).isSameInstanceAs(ACCOUNT_ID);
+  }
+
+  @Test
+  public void whenAssembling_thenReturnParkingStickerWithAccountId() {
+    parkingStickerDto = aParkingStickerDto().withParkingArea(PARKING_AREA).build();
+
+    ParkingSticker parkingSticker = parkingStickerAssembler.assemble(parkingStickerDto);
+
+    Truth.assertThat(parkingSticker.getParkingAreaCode().toString()).isEqualTo(PARKING_AREA);
+  }
+}
