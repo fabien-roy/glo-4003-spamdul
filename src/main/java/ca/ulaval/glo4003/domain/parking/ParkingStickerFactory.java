7b78491569df0127e2d26b940676ada80fb7eddb
Fabien Roy <fabienroy28@gmail.com>
2020-09-21 11:18:42 -0400

    [FEATURE] Add logic to parking code creation

--- src/main/java/ca/ulaval/glo4003/domain/parking/ParkingStickerFactory.java
+++ src/main/java/ca/ulaval/glo4003/domain/parking/ParkingStickerFactory.java
@@ -1,8 +1,15 @@
 package ca.ulaval.glo4003.domain.parking;
 
 public class ParkingStickerFactory {
+  private final ParkingStickerCodeGenerator parkingStickerCodeGenerator;
+
+  public ParkingStickerFactory(ParkingStickerCodeGenerator parkingStickerCodeGenerator) {
+    this.parkingStickerCodeGenerator = parkingStickerCodeGenerator;
+  }
+
   public ParkingSticker create(ParkingSticker parkingSticker) {
-    // TODO : ParkingStickerFactory::create(ParkingSticker)
+    ParkingStickerCode code = parkingStickerCodeGenerator.generate();
+    parkingSticker.setCode(code);
     return parkingSticker;
   }
 }
---
+++ src/test/java/ca/ulaval/glo4003/domain/parking/ParkingStickerFactoryTest.java
@@ -0,0 +1,37 @@
+package ca.ulaval.glo4003.domain.parking;
+
+import static ca.ulaval.glo4003.domain.parking.helpers.ParkingStickerBuilder.aParkingSticker;
+import static ca.ulaval.glo4003.domain.parking.helpers.ParkingStickerObjectMother.createParkingStickerCode;
+
+import com.google.common.truth.Truth;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ParkingStickerFactoryTest {
+  private static final ParkingStickerCode PARKING_STICKER_CODE = createParkingStickerCode();
+
+  @Mock private ParkingStickerCodeGenerator parkingStickerCodeGenerator;
+
+  private ParkingSticker parkingSticker = aParkingSticker().build();
+
+  private ParkingStickerFactory parkingStickerFactory;
+
+  @Before
+  public void setUp() {
+    parkingStickerFactory = new ParkingStickerFactory(parkingStickerCodeGenerator);
+
+    BDDMockito.given(parkingStickerCodeGenerator.generate()).willReturn(PARKING_STICKER_CODE);
+  }
+
+  @Test
+  public void whenCreating_thenGeneratorCode() {
+    ParkingSticker createdParkingSticker = parkingStickerFactory.create(parkingSticker);
+
+    Truth.assertThat(createdParkingSticker.getCode()).isSameInstanceAs(PARKING_STICKER_CODE);
+  }
+}
