68f0923fde877916d2b28c43b6fd87a3e7a79d9e
Fabien Roy <fabienroy28@gmail.com>
2020-09-20 22:28:43 -0400

    [FEATURE] Add test for parking area creation

---
+++ src/main/java/ca/ulaval/glo4003/domain/parking/ParkingStickerFactory.java
@@ -0,0 +1,8 @@
+package ca.ulaval.glo4003.domain.parking;
+
+public class ParkingStickerFactory {
+  public ParkingSticker create(ParkingSticker parkingSticker) {
+    // TODO : ParkingStickerFactory::create(ParkingSticker)
+    return parkingSticker;
+  }
+}
