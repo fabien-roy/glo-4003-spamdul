8dc6d7c8bf7f17507f86ec8263d85d61bdc52962
Olivier Gingras <olivier.gingras.1@ulaval.ca>
2020-09-22 19:43:20 -0400

    [FIX] Format code

--- src/main/java/ca/ulaval/glo4003/domain/user/exception/InvalidNameException.java
+++ src/main/java/ca/ulaval/glo4003/domain/user/exception/InvalidNameException.java
@@ -1,6 +1,5 @@
 package ca.ulaval.glo4003.domain.user.exception;
 
-
 public class InvalidNameException extends InvalidUserException {
   private static final String error = "Invalid name";
   private static final String description = "Name can't be empty";
