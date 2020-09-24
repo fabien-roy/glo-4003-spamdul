5fc9d7f74d245ab57c7e4034a2f23716d6433d10
Fabien Roy <fabienroy28@gmail.com>
2020-09-24 19:07:33 -0400

    [FEATURE] Add doc for postal code

---
+++ src/main/java/ca/ulaval/glo4003/domain/location/PostalCodeAssembler.java
@@ -0,0 +1,8 @@
+package ca.ulaval.glo4003.domain.location;
+
+public class PostalCodeAssembler {
+  public PostalCode assemble(String postalCode) {
+    // TODO : PostalCodeAssembler::assemble
+    return new PostalCode(postalCode);
+  }
+}
