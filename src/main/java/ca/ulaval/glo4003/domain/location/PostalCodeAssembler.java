78d3c2315041d87953c59820d9cfbc3d8c680c01
Fabien Roy <fabienroy28@gmail.com>
2020-09-24 19:16:19 -0400

    [FEATURE] Add postal code assembler

--- src/main/java/ca/ulaval/glo4003/domain/location/PostalCodeAssembler.java
+++ src/main/java/ca/ulaval/glo4003/domain/location/PostalCodeAssembler.java
@@ -1,8 +1,16 @@
 package ca.ulaval.glo4003.domain.location;
 
+import ca.ulaval.glo4003.domain.location.exception.InvalidPostalCodeException;
+import java.util.regex.Pattern;
+
 public class PostalCodeAssembler {
+  private static final Pattern PATTERN = Pattern.compile("[A-Z][0-9][A-Z] [0-9][A-Z][0-9]");
+
   public PostalCode assemble(String postalCode) {
-    // TODO : PostalCodeAssembler::assemble
+    if (!PATTERN.matcher(postalCode).matches()) {
+      throw new InvalidPostalCodeException();
+    }
+
     return new PostalCode(postalCode);
   }
 }
