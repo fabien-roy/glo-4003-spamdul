b57fb227ad5c7ac53725d3aeb4bb2b25898f87e8
Philippe <tiphil1@live.ca>
2020-09-24 21:37:46 -0400

    Added missing test to CarAssembler

--- src/test/java/ca/ulaval/glo4003/domain/car/CarAssemblerTest.java
+++ src/test/java/ca/ulaval/glo4003/domain/car/CarAssemblerTest.java
@@ -1,5 +1,48 @@
 package ca.ulaval.glo4003.domain.car;
 
-class CarAssemblerTest {
-  // TODO : Add test here
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.verify;
+
+import ca.ulaval.glo4003.api.car.dto.CarDTO;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CarAssemblerTest {
+
+  private static final String MANUFACTURER = "Toyota";
+  private static final String MODEL = "Camry";
+  private static final int YEAR = 1300;
+  private static final String LICENSE_PLATE = "SPEED";
+
+  private CarAssembler carAssembler;
+  private CarDTO carDTO;
+  @Mock private CarValidator carValidator;
+
+  @Before
+  public void setup() {
+    carDTO = new CarDTO(MANUFACTURER, MODEL, YEAR, LICENSE_PLATE);
+
+    carAssembler = new CarAssembler(carValidator);
+  }
+
+  @Test
+  public void whenCreatingCar_shouldCallCarValidator() {
+    carAssembler.create(carDTO);
+
+    verify(carValidator).validate(carDTO);
+  }
+
+  @Test
+  public void whenCreatingCar_shouldReturnACar() {
+    Car car = carAssembler.create(carDTO);
+
+    assertThat(car.getManufacturer().equals(MANUFACTURER));
+    assertThat(car.getModel().equals(MODEL));
+    assertThat(car.getYear() == YEAR);
+    assertThat(car.getLicensePlate().equals(LICENSE_PLATE));
+  }
 }
