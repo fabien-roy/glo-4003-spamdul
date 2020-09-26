ec8c6343915967c400b737f52f35e78bfdd975b1
Lapro15 <laurence.prov1@gmail.com>
2020-09-26 16:54:25 -0400

    fix mapper and inject

--- src/main/java/ca/ulaval/glo4003/serverConfiguration/ApplicationBinder.java
+++ src/main/java/ca/ulaval/glo4003/serverConfiguration/ApplicationBinder.java
@@ -10,11 +10,14 @@ import ca.ulaval.glo4003.domain.account.*;
 import ca.ulaval.glo4003.domain.car.CarAssembler;
 import ca.ulaval.glo4003.domain.car.CarService;
 import ca.ulaval.glo4003.domain.car.CarValidator;
+import ca.ulaval.glo4003.domain.location.PostalCodeAssembler;
 import ca.ulaval.glo4003.domain.parking.*;
 import ca.ulaval.glo4003.domain.time.CustomDateAssembler;
 import ca.ulaval.glo4003.domain.user.UserAssembler;
+import ca.ulaval.glo4003.domain.user.UserService;
 import ca.ulaval.glo4003.infrastructure.account.AccountRepositoryInMemory;
 import ca.ulaval.glo4003.infrastructure.parking.ParkingAreaRepositoryInMemory;
+import ca.ulaval.glo4003.infrastructure.parking.ParkingStickerRepositoryInMemory;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
 public class ApplicationBinder extends AbstractBinder {
@@ -23,41 +26,53 @@ public class ApplicationBinder extends AbstractBinder {
     configureUser();
     configureParking();
     configureCar();
+    configureAccount();
+    configureLocation();
+    configureTime();
   }
 
   private void configureUser() {
-    // bindAsContract(UserResource.class);
     bind(UserResourceImplementation.class).to(UserResource.class);
 
     bindAsContract(UserAssembler.class);
+    bindAsContract(UserService.class);
+  }
 
-    bindAsContract(CustomDateAssembler.class);
+  private void configureAccount() {
+    bind(AccountRepositoryInMemory.class).to(AccountRepository.class);
+
     bindAsContract(AccountIdGenerator.class);
     bindAsContract(AccountIdAssembler.class);
     bindAsContract(AccountFactory.class);
     bindAsContract(AccountService.class);
-
-    // bindAsContract(AccountRepository.class);
-    bind(AccountRepositoryInMemory.class).to(AccountRepository.class);
   }
 
   private void configureParking() {
-    // bindAsContract(ParkingResource.class);
     bind(ParkingResourceImplementation.class).to(ParkingResource.class);
-
-    // bindAsContract(ParkingAreaRepository.class);
     bind(ParkingAreaRepositoryInMemory.class).to(ParkingAreaRepository.class);
+    bind(ParkingStickerRepositoryInMemory.class).to(ParkingStickerRepository.class);
 
     bindAsContract(ParkingStickerAssembler.class);
     bindAsContract(ParkingStickerCodeAssembler.class);
     bindAsContract(ParkingStickerCodeGenerator.class);
     bindAsContract(ParkingStickerFactory.class);
+    bindAsContract(ParkingService.class);
+    bindAsContract(ParkingStickerFactory.class);
   }
 
   private void configureCar() {
     bind(CarResourceImplementation.class).to(ContactResource.class);
+
     bindAsContract(CarValidator.class);
     bindAsContract(CarAssembler.class);
     bindAsContract(CarService.class);
+  }
+
+  private void configureLocation() {
+    bindAsContract(PostalCodeAssembler.class);
+  }
+
+  private void configureTime() {
+    bindAsContract(CustomDateAssembler.class);
   }
 }
