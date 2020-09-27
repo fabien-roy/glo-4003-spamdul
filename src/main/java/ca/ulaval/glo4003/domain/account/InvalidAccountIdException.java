8ef516c6a90fcf59f3187d12b1850cb9cabc6b79
Lapro15 <laurence.prov1@gmail.com>
2020-09-27 16:28:10 -0400

    unwanted class

--- src/main/java/ca/ulaval/glo4003/domain/account/InvalidAccountIdException.java
+++
@@ -1,7 +0,0 @@
-package ca.ulaval.glo4003.domain.account;
-
-public class InvalidAccountIdException extends RuntimeException {
-  public InvalidAccountIdException() {
-    super("Invalid account id");
-  }
-}
