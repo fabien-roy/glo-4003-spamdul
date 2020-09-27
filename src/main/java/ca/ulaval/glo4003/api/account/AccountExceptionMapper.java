1c5a935832c962ff9774af3382a043684c72785d
Fabien Roy <fabienroy28@gmail.com>
2020-09-26 20:05:57 -0400

    [FIX] Refactor account exception mapper

--- src/main/java/ca/ulaval/glo4003/api/account/AccountExceptionMapper.java
+++ src/main/java/ca/ulaval/glo4003/api/account/AccountExceptionMapper.java
@@ -1,5 +1,6 @@
-package ca.ulaval.glo4003.api.exceptionMapper;
+package ca.ulaval.glo4003.api.account;
 
+import ca.ulaval.glo4003.api.interfaces.dto.ErrorDto;
 import ca.ulaval.glo4003.domain.account.exception.AccountException;
 import ca.ulaval.glo4003.domain.account.exception.InvalidAccountIdException;
 import ca.ulaval.glo4003.domain.account.exception.NotFoundAccountException;
@@ -11,28 +12,22 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class AccountExceptionMapper implements ExceptionMapper<AccountException> {
 
-  public static class AccountExceptionResponse {
-    public String error;
-    public String description;
-  }
-
   @Override
   public Response toResponse(AccountException exception) {
     Response.Status responseStatus = Response.Status.INTERNAL_SERVER_ERROR;
 
     if (exception instanceof InvalidAccountIdException) {
       responseStatus = Response.Status.BAD_REQUEST;
-    }
-    if (exception instanceof NotFoundAccountException) {
+    } else if (exception instanceof NotFoundAccountException) {
       responseStatus = Response.Status.NOT_FOUND;
     }
 
-    AccountExceptionResponse accountExceptionResponse = new AccountExceptionResponse();
-    accountExceptionResponse.error = exception.error;
-    accountExceptionResponse.description = exception.description;
+    ErrorDto errorDto = new ErrorDto();
+    errorDto.error = exception.error;
+    errorDto.description = exception.description;
 
     return Response.status(responseStatus)
-        .entity(accountExceptionResponse)
+        .entity(errorDto)
         .type(MediaType.APPLICATION_JSON)
         .build();
   }
