faf943d9fe350775ebf2d807b24f018c7208110c
Fabien Roy <fabienroy28@gmail.com>
2020-09-29 11:44:33 -0400

    [FEATURE] Add logic and tests of email address assembler

--- src/main/java/ca/ulaval/glo4003/domain/communication/EmailAddressAssembler.java
+++ src/main/java/ca/ulaval/glo4003/domain/communication/EmailAddressAssembler.java
@@ -1,12 +1,16 @@
 package ca.ulaval.glo4003.domain.communication;
 
+import ca.ulaval.glo4003.domain.communication.exception.InvalidEmailAddressException;
 import java.util.regex.Pattern;
 
 public class EmailAddressAssembler {
   private static final Pattern PATTERN = Pattern.compile("[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}");
 
   public EmailAddress assemble(String emailAddress) {
-    // TODO : EmailAddressAssembler.assembler
-    return null;
+    if (!PATTERN.matcher(emailAddress).matches()) {
+      throw new InvalidEmailAddressException();
+    }
+
+    return new EmailAddress(emailAddress);
   }
 }
