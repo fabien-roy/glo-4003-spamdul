ea03923fdc34d0473f78603e965f9a92912f0108
Lapro15 <laurence.prov1@gmail.com>
2020-09-29 21:25:36 -0400

    last file tested

--- src/test/java/ca/ulaval/glo4003/domain/offense/OffenseAssemblerTest.java
+++ src/test/java/ca/ulaval/glo4003/domain/offense/OffenseAssemblerTest.java
@@ -44,23 +44,23 @@ public class OffenseAssemblerTest {
 
   @Test
   public void whenAssembling_thenReturnOffenseDtoWithReasonText() {
-    OffenseDto offenseDto = offenseAssembler.assemble(this.offense);
+    OffenseDto offense = offenseAssembler.assemble(this.offense);
 
-    Truth.assertThat(offenseDto.reasonText).isEqualTo(REASON_TEXT);
+    Truth.assertThat(offense.infraction).isEqualTo(REASON_TEXT);
   }
 
   @Test
   public void whenAssembling_thenReturnOffenseDtoWithReasonCode() {
-    OffenseDto offenseDto = offenseAssembler.assemble(this.offense);
+    OffenseDto offense = offenseAssembler.assemble(this.offense);
 
-    Truth.assertThat(offenseDto.reasonCode).isEqualTo(REASON_CODE);
+    Truth.assertThat(offense.code).isEqualTo(REASON_CODE);
   }
 
   @Test
   public void whenAssembling_thenReturnOffenseDtoWithAmount() {
-    OffenseDto offenseDto = offenseAssembler.assemble(this.offense);
+    OffenseDto offense = offenseAssembler.assemble(this.offense);
 
-    Truth.assertThat(offenseDto.amount).isEqualTo(AMOUNT);
+    Truth.assertThat(offense.montant).isEqualTo(AMOUNT);
   }
 
   @Test
@@ -69,8 +69,8 @@ public class OffenseAssemblerTest {
     manyOffenses.add(offense);
     manyOffenses.add(offense);
 
-    List<OffenseDto> assembledOffenseDtos = offenseAssembler.assembleMany(manyOffenses);
+    List<OffenseDto> assembledOffenses = offenseAssembler.assembleMany(manyOffenses);
 
-    Truth.assertThat(assembledOffenseDtos.size()).isEqualTo(2);
+    Truth.assertThat(assembledOffenses.size()).isEqualTo(2);
   }
 }
--- src/test/java/ca/ulaval/glo4003/domain/offense/OffenseValidationAssemblerTest.java
+++ src/test/java/ca/ulaval/glo4003/domain/offense/OffenseValidationAssemblerTest.java
@@ -1,7 +1,9 @@
 package ca.ulaval.glo4003.domain.offense;
 
-
-import static org.junit.Assert.*;
+import static ca.ulaval.glo4003.api.offense.helpers.OffenseValidationDtoBuilder.anOffenseValidationDto;
+import static ca.ulaval.glo4003.domain.parking.helpers.ParkingAreaMother.createParkingAreaCode;
+import static ca.ulaval.glo4003.domain.parking.helpers.ParkingStickerMother.createParkingStickerCode;
+import static ca.ulaval.glo4003.domain.time.helpers.TimeOfDayMother.createTimeOfDay;
 import static org.mockito.Mockito.when;
 
 import ca.ulaval.glo4003.api.offense.dto.OffenseValidationDto;
@@ -9,17 +11,23 @@ import ca.ulaval.glo4003.domain.parking.ParkingAreaCode;
 import ca.ulaval.glo4003.domain.parking.ParkingAreaCodeAssembler;
 import ca.ulaval.glo4003.domain.parking.ParkingStickerCode;
 import ca.ulaval.glo4003.domain.parking.ParkingStickerCodeAssembler;
+import ca.ulaval.glo4003.domain.time.TimeOfDay;
 import com.google.common.truth.Truth;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OffenseValidationAssemblerTest {
-  private ParkingStickerCodeAssembler parkingStickerCodeAssembler;
-  private ParkingAreaCodeAssembler parkingAreaCodeAssembler;
-  private ParkingStickerCode parkingStickerCode;
+  private static final ParkingStickerCode PARKING_STICKER_CODE = createParkingStickerCode();
+  private static final ParkingAreaCode PARKING_AREA_CODE = createParkingAreaCode();
+  private static final TimeOfDay TIME_OF_DAY = createTimeOfDay();
+
+  @Mock private ParkingStickerCodeAssembler parkingStickerCodeAssembler;
+  @Mock private ParkingAreaCodeAssembler parkingAreaCodeAssembler;
+
   private OffenseValidationDto offenseValidationDto;
   private OffenseValidationAssembler offenseValidationAssembler;
 
@@ -28,18 +36,32 @@ public class OffenseValidationAssemblerTest {
     offenseValidationAssembler =
         new OffenseValidationAssembler(parkingStickerCodeAssembler, parkingAreaCodeAssembler);
 
-    offenseValidationDto = new OffenseValidationDto();
+    offenseValidationDto = anOffenseValidationDto().build();
 
     when(parkingStickerCodeAssembler.assemble(offenseValidationDto.parkingStickerCode))
-        .thenReturn(parkingStickerCode);
+        .thenReturn(PARKING_STICKER_CODE);
     when(parkingAreaCodeAssembler.assemble(offenseValidationDto.parkingArea))
-        .thenReturn(new ParkingAreaCode("2"));
+        .thenReturn(PARKING_AREA_CODE);
   }
 
   @Test
   public void whenAssembling_thenOffenseValidationWithParkingStickerCodeIsReturned() {
     OffenseValidation offenseValidation = offenseValidationAssembler.assemble(offenseValidationDto);
 
-    Truth.assertThat(offenseValidation.getParkingStickerCode()).isEqualTo(parkingStickerCode);
+    Truth.assertThat(offenseValidation.getParkingStickerCode()).isEqualTo(PARKING_STICKER_CODE);
+  }
+
+  @Test
+  public void whenAssembling_thenOffenseValidationWithParkingAreaCodeIsReturned() {
+    OffenseValidation offenseValidation = offenseValidationAssembler.assemble(offenseValidationDto);
+
+    Truth.assertThat(offenseValidation.getParkingAreaCode()).isEqualTo(PARKING_AREA_CODE);
+  }
+
+  @Test
+  public void whenAssembling_thenOffenseValidationWithTimeOfDayIsReturned() {
+    OffenseValidation offenseValidation = offenseValidationAssembler.assemble(offenseValidationDto);
+
+    Truth.assertThat(offenseValidation.getTimeOfDay()).isEqualTo(TIME_OF_DAY);
   }
 }
