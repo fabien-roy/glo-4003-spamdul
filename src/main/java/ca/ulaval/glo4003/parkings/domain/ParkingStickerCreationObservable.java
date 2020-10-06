7102fe7ed3227903835bd45d0eee8f038608e2c2
Fabien Roy <fabienroy28@gmail.com>
2020-10-05 21:43:13 -0400

    [FEATURE] Test parking sticker creation observable

--- src/main/java/ca/ulaval/glo4003/parkings/domain/ParkingStickerCreationObservable.java
+++ src/main/java/ca/ulaval/glo4003/parkings/domain/ParkingStickerCreationObservable.java
@@ -3,7 +3,6 @@ package ca.ulaval.glo4003.parkings.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO : Test ParkingStickerCreationObservable
 public class ParkingStickerCreationObservable {
   private List<ParkingStickerCreationObserver> observers = new ArrayList<>();
 
