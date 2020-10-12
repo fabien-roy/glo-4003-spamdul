c749b1f0bcb0d5a3401f5804ded78b6394ac9a98
Olivier Gingras <olivier.gingras.1@ulaval.ca>
2020-10-12 17:44:07 -0400

    [REFACTOR] Changed exception description

--- src/main/java/ca/ulaval/glo4003/funds/exception/AmountDueExceededException.java
+++ src/main/java/ca/ulaval/glo4003/funds/exception/AmountDueExceededException.java
@@ -1,8 +1,8 @@
 package ca.ulaval.glo4003.funds.exception;
 
 public class AmountDueExceededException extends FundsException {
-  private static final String ERROR = "Too much money";
-  private static final String DESCRIPTION = "There is too much money to paid this bill";
+  private static final String ERROR = "Amount due exceeded";
+  private static final String DESCRIPTION = "The amount paid exceeds the amount requested";
 
   public AmountDueExceededException() {
     super(ERROR, DESCRIPTION);
