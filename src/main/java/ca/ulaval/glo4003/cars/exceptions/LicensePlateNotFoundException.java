62cc652210f03b6e2200ebfc7b4d8d009f207b5c
Olivier Gingras <olivier.gingras.1@ulaval.ca>
2020-10-12 17:37:51 -0400

    [REFACTOR] Changed exception name

--- src/main/java/ca/ulaval/glo4003/cars/exceptions/LicensePlateNotFoundException.java
+++ src/main/java/ca/ulaval/glo4003/cars/exceptions/LicensePlateNotFoundException.java
@@ -1,10 +1,10 @@
 package ca.ulaval.glo4003.cars.exceptions;
 
-public class NotFoundLicensePlate extends CarException {
+public class LicensePlateNotFoundException extends CarException {
   private static final String ERROR = "License Plate not found";
   private static final String DESCRIPTION = "There is no car with this license plate";
 
-  public NotFoundLicensePlate() {
+  public LicensePlateNotFoundException() {
     super(ERROR, DESCRIPTION);
   }
 }
