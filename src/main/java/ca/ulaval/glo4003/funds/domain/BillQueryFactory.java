f0f85d9b6d1799870e486d326ab7b823612f9232
Philippe <tiphil1@live.ca>
2020-10-15 19:22:19 -0400

    removed a todo and an empty test file

--- src/main/java/ca/ulaval/glo4003/funds/domain/BillQueryFactory.java
+++ src/main/java/ca/ulaval/glo4003/funds/domain/BillQueryFactory.java
@@ -17,7 +17,7 @@ public class BillQueryFactory {
   }
 
   public BillQuery create(Map<String, List<String>> params) {
-    BillQueryBuilder builder = billQueryBuilder.emptyBillQuery(); // TODO : emptyBillQuery here!
+    BillQueryBuilder builder = billQueryBuilder.emptyBillQuery();
 
     for (BillQueryParamAssembler queryParamAssembler : queryParamAssemblers)
       builder = queryParamAssembler.assemble(builder, params);
