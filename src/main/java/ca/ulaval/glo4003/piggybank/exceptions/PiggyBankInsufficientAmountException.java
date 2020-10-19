37cd7e23f02589af80d725089409a099e9cd442f
raphaelbouch <31938346+raphaelbouch@users.noreply.github.com>
2020-10-19 10:40:49 -0400

    Update PiggyBankInsufficientAmountException.java
    [FIX] fix typo in exception description

--- src/main/java/ca/ulaval/glo4003/piggybank/exceptions/PiggyBankInsufficientAmountException.java
+++ src/main/java/ca/ulaval/glo4003/piggybank/exceptions/PiggyBankInsufficientAmountException.java
@@ -2,7 +2,7 @@ package ca.ulaval.glo4003.piggybank.exceptions;
 
 public class PiggyBankInsufficientAmountException extends PiggyBankException {
   private static final String ERROR = "Insufficient amount ";
-  private static final String DESCRIPTION = "Piggy bank has insufficient amount";
+  private static final String DESCRIPTION = "Piggy bank has an insufficient amount";
 
   public PiggyBankInsufficientAmountException() {
     super(ERROR, DESCRIPTION);
