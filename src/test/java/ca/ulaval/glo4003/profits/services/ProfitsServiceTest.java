bb207702f0fc46971276401e23e563f945ba0f1c
Olivier Gingras <olivier.gingras.1@ulaval.ca>
2020-10-24 20:23:34 -0400

    [FIX] Added service test

--- src/test/java/ca/ulaval/glo4003/profits/services/ProfitsServiceTest.java
+++ src/test/java/ca/ulaval/glo4003/profits/services/ProfitsServiceTest.java
@@ -1,7 +1,7 @@
 package ca.ulaval.glo4003.profits.services;
 
 import static ca.ulaval.glo4003.funds.helpers.MoneyMother.createMoney;
-import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import ca.ulaval.glo4003.funds.domain.Bill;
@@ -13,8 +13,10 @@ import ca.ulaval.glo4003.funds.services.BillProfitsCalculator;
 import ca.ulaval.glo4003.funds.services.BillService;
 import ca.ulaval.glo4003.profits.assemblers.ProfitByConsumptionTypeAssembler;
 import ca.ulaval.glo4003.profits.assemblers.ProfitsAssembler;
+import ca.ulaval.glo4003.profits.domain.ProfitByConsumptionType;
 import ca.ulaval.glo4003.profits.domain.ProfitByConsumptionTypeFactory;
 import ca.ulaval.glo4003.times.helpers.CustomDateMother;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,9 +32,10 @@ public class ProfitsServiceTest {
 
   private ProfitsService profitsService;
 
-  private ProfitsAssembler profitsAssembler;
-  private List<Bill> bills;
+  private List<Bill> bills = new ArrayList<>();
+  private List<ProfitByConsumptionType> profitByConsumptionTypes = new ArrayList<>();
 
+  @Mock private ProfitsAssembler profitsAssembler;
   @Mock private BillQueryParamsAssembler billQueryParamsAssembler;
   @Mock private BillQueryParams billQueryParams;
   @Mock private BillService billService;
@@ -42,7 +45,6 @@ public class ProfitsServiceTest {
 
   @Before
   public void setup() {
-    profitsAssembler = new ProfitsAssembler();
     profitsService =
         new ProfitsService(
             profitsAssembler,
@@ -65,20 +67,28 @@ public class ProfitsServiceTest {
   public void whenGettingProfitsFromParkingSticker_shouldAssembleProfits() {
     profitsService.getParkingStickerProfits(A_YEAR);
 
-    assertThat(profitsAssembler.assemble(AN_AMOUNT));
+    verify(profitsAssembler).assemble(AN_AMOUNT);
   }
 
   @Test
   public void whenGettingProfitsFromOffsense_shouldAssembleProfits() {
     profitsService.getOffenseProfits(A_YEAR);
 
-    assertThat(profitsAssembler.assemble(AN_AMOUNT));
+    verify(profitsAssembler).assemble(AN_AMOUNT);
   }
 
   @Test
   public void whenGettingProfitsFromAccessPass_shouldAssembleProfits() {
     profitsService.getAccessPassProfits(A_YEAR);
 
-    assertThat(profitsAssembler.assemble(AN_AMOUNT));
+    verify(profitsAssembler).assemble(AN_AMOUNT);
+  }
+
+  @Test
+  public void
+      whenGettingProfitsFromAccessPassByConsumptionTest_shouldAssembleProfitsByConsumptionType() {
+    profitsService.getAccessPassProfitsByConsumptionType(A_YEAR);
+
+    verify(profitByConsumptionTypeAssembler).assembleMany(profitByConsumptionTypes);
   }
 }
