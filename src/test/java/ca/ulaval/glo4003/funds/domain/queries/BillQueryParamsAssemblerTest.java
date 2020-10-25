c113004ee6d6092b5aa419dfe5224661a6c729e3
Olivier Gingras <olivier.gingras.1@ulaval.ca>
2020-10-24 20:48:15 -0400

    [FIX] Added classes to test

---
+++ src/test/java/ca/ulaval/glo4003/funds/domain/queries/BillQueryParamsAssemblerTest.java
@@ -0,0 +1,5 @@
+package ca.ulaval.glo4003.funds.domain.queries;
+
+public class BillQueryParamsAssemblerTest {
+  // TODO
+}
---
+++ src/test/java/ca/ulaval/glo4003/profits/assemblers/ProfitByConsumptionTypeFactory.java
@@ -0,0 +1,5 @@
+package ca.ulaval.glo4003.profits.assemblers;
+
+public class ProfitByConsumptionTypeFactory {
+  // TODO
+}
