b5ae17ceaa9795d4411325f965ddb1dfb36e1a0f
Fabien Roy <fabienroy28@gmail.com>
2020-11-02 11:51:04 -0500

    [FEATURE] Add missing interface for metric builder

--- src/main/java/ca/ulaval/glo4003/reports/domain/dimensions/ReportDimensionBuilder.java
+++ src/main/java/ca/ulaval/glo4003/reports/domain/dimensions/ReportDimensionBuilder.java
@@ -2,6 +2,7 @@ package ca.ulaval.glo4003.reports.domain.dimensions;
 
 import java.util.List;
 
+// TODO : Will this builder really be for many dimensions or only a single one?
 public interface ReportDimensionBuilder {
 
   ReportDimensionBuilder someDimensions();
