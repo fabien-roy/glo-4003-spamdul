d72611b6cb972f0d89e1f8856ab46a6237fc7c9f
Fabien Roy <fabienroy28@gmail.com>
2020-11-06 13:51:36 -0500

    [FEATURE] Add logic to report scope

---
+++ src/main/java/ca/ulaval/glo4003/times/domain/TimeCalendar.java
@@ -0,0 +1,10 @@
+package ca.ulaval.glo4003.times.domain;
+
+public abstract class TimeCalendar implements Comparable<TimeCalendar> {
+  // TODO : #248
+
+  public TimePeriod toPeriod() {
+    // TODO
+    return null;
+  }
+}
