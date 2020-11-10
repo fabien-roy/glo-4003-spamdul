d6b4fef914cada6fd1161324cc06eef2f3cb8974
Fabien Roy <fabienroy28@gmail.com>
2020-11-09 21:22:33 -0500

    [FIX] Make random value random

--- src/test/java/ca/ulaval/glo4003/times/helpers/TimeOfDayMother.java
+++ src/test/java/ca/ulaval/glo4003/times/helpers/TimeOfDayMother.java
@@ -1,11 +1,13 @@
 package ca.ulaval.glo4003.times.helpers;
 
 import ca.ulaval.glo4003.times.domain.TimeOfDay;
+import com.github.javafaker.Faker;
 import java.time.LocalTime;
 
 public class TimeOfDayMother {
   public static TimeOfDay createTimeOfDay() {
-    // TODO : This isn't random at all.
-    return new TimeOfDay(LocalTime.parse("10:30"));
+    int hours = Faker.instance().number().numberBetween(0, 23);
+    int minutes = Faker.instance().number().numberBetween(0, 59);
+    return new TimeOfDay(LocalTime.of(hours, minutes));
   }
 }
