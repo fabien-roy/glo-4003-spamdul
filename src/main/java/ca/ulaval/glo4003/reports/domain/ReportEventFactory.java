9a2c67a1d461e995f5bf14e56db45f908ffd266c
Fabien Roy <fabienroy28@gmail.com>
2020-11-15 16:17:56 -0500

    [FEATURE] Test report event factory

--- src/main/java/ca/ulaval/glo4003/reports/domain/ReportEventFactory.java
+++ src/main/java/ca/ulaval/glo4003/reports/domain/ReportEventFactory.java
@@ -4,7 +4,6 @@ import ca.ulaval.glo4003.cars.domain.ConsumptionType;
 import ca.ulaval.glo4003.funds.domain.Money;
 import ca.ulaval.glo4003.times.domain.CustomDateTime;
 
-// TODO : Test ReportEventFactory
 public class ReportEventFactory {
 
   public ReportEvent create(ReportEventType type, Money profits) {
