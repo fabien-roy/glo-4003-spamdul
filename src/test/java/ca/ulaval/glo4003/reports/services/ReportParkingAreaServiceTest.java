6006a014e9d6783182b5a14174dba804c22258cb
Fabien Roy <fabienroy28@gmail.com>
2020-11-18 16:16:50 -0500

    [FIX] Cleanup report parking area service tests

--- src/test/java/ca/ulaval/glo4003/reports/services/ReportParkingAreaServiceTest.java
+++ src/test/java/ca/ulaval/glo4003/reports/services/ReportParkingAreaServiceTest.java
@@ -1,20 +1,23 @@
 package ca.ulaval.glo4003.reports.services;
 
+import static ca.ulaval.glo4003.reports.helpers.ReportPeriodBuilder.aReportPeriod;
+import static ca.ulaval.glo4003.reports.helpers.ReportPeriodDtoBuilder.aReportPeriodDto;
 import static ca.ulaval.glo4003.reports.helpers.ReportTypeMother.createReportType;
-import static org.mockito.Mockito.verify;
+import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.when;
 
+import ca.ulaval.glo4003.reports.api.dto.ReportPeriodDto;
 import ca.ulaval.glo4003.reports.assemblers.ReportPeriodAssembler;
 import ca.ulaval.glo4003.reports.domain.*;
 import ca.ulaval.glo4003.times.helpers.CustomDateTimeMother;
 import java.util.Collections;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-// TODO : Refactor this test class and remove most of verify()
 @RunWith(MockitoJUnitRunner.class)
 public class ReportParkingAreaServiceTest {
   @Mock private ReportRepository reportRepository;
@@ -22,10 +25,11 @@ public class ReportParkingAreaServiceTest {
   @Mock private ReportQueryFactory reportQueryFactory;
   @Mock private ReportSummaryBuilder reportSummaryBuilder;
   @Mock private ReportQuery reportQuery;
-  @Mock private ReportPeriod reportPeriod; // TODO : Why mock ReportPeriod?
 
   private ReportParkingAreaService reportParkingAreaService;
 
+  private final ReportPeriod reportPeriod = aReportPeriod().build();
+  private final ReportPeriodDto reportPeriodDto = aReportPeriodDto().build();
   private final String reportType = createReportType().toString();
   private final String month = CustomDateTimeMother.createMonth().toString();
 
@@ -38,26 +42,16 @@ public class ReportParkingAreaServiceTest {
     when(reportQueryFactory.create(ReportType.get(reportType), month)).thenReturn(reportQuery);
     when(reportRepository.getPeriods(reportQuery))
         .thenReturn(Collections.singletonList(reportPeriod));
+    when(reportPeriodAssembler.assembleMany(Collections.singletonList(reportPeriod)))
+        .thenReturn(Collections.singletonList(reportPeriodDto));
   }
 
   @Test
-  public void whenGettingReports_thenCreateReportQuery() {
-    reportParkingAreaService.getAllParkingAreaReports(reportType, month);
+  public void whenGettingParkingAreaReports_thenGetParkingAreaReports() {
+    List<ReportPeriodDto> reportPeriodDtos =
+        reportParkingAreaService.getAllParkingAreaReports(reportType, month);
 
-    verify(reportQueryFactory).create(ReportType.get(reportType), month);
-  }
-
-  @Test
-  public void whenGettingReports_thenGetReportsPeriods() {
-    reportParkingAreaService.getAllParkingAreaReports(reportType, month);
-
-    verify(reportRepository).getPeriods(reportQuery);
-  }
-
-  @Test
-  public void whenGettingReports_thenAssembleReports() {
-    reportParkingAreaService.getAllParkingAreaReports(reportType, month);
-
-    verify(reportPeriodAssembler).assembleMany(Collections.singletonList(reportPeriod));
+    assertThat(reportPeriodDtos).hasSize(1);
+    assertThat(reportPeriodDtos.get(0)).isSameInstanceAs(reportPeriodDto);
   }
 }
