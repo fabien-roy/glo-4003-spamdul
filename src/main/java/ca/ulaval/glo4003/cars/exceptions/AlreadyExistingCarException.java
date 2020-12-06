75dbc6b841157c6654ecbb11675726f0847ef179
Fabien Roy <fabienroy28@gmail.com>
2020-12-06 12:31:12 -0500

    [DOC] Change expected error messages in E2E tests

--- src/main/java/ca/ulaval/glo4003/cars/exceptions/AlreadyExistingCarException.java
+++ src/main/java/ca/ulaval/glo4003/cars/exceptions/AlreadyExistingCarException.java
@@ -2,7 +2,7 @@ package ca.ulaval.glo4003.cars.exceptions;
 
 public class AlreadyExistingCarException extends CarException {
   private static final String ERROR = "Already existing car";
-  private static final String DESCRIPTION = "Car must not already exist";
+  private static final String DESCRIPTION = "There already exists a car with this license plate";
 
   public AlreadyExistingCarException() {
     super(ERROR, DESCRIPTION);
