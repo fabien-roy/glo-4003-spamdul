ed4ba31e3238fd527fd25179a6d109c5426db79b
Fabien Roy <fabienroy28@gmail.com>
2020-12-06 21:25:33 -0500

    Â[FEATURE] Test ReportScopeFactory

--- src/main/java/ca/ulaval/glo4003/reports/domain/scopes/ReportScopeFactory.java
+++ src/main/java/ca/ulaval/glo4003/reports/domain/scopes/ReportScopeFactory.java
@@ -4,17 +4,14 @@ import ca.ulaval.glo4003.times.domain.TimeMonth;
 import ca.ulaval.glo4003.times.domain.TimeYear;
 
 public class ReportScopeFactory {
-  // TODO #326 : Test this
   public ReportScope createYearlyScope(int year) {
     return new YearlyScope(new TimeYear(year).toPeriod());
   }
 
-  // TODO #326 : Test this
   public ReportScope createMonthlyScope() {
     return new MonthlyScope(TimeYear.now().toPeriod());
   }
 
-  // TODO #326 : Test this
   public ReportScope createDailyScope(String month) {
     return new DailyScope(new TimeMonth(month).toPeriod());
   }
