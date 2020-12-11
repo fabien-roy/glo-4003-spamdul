a0390f1fac0fed3a48002f7712a060db0326bf09
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 18:40:27 -0500

    [ENHANCEMENT] Refactor offenses exceptions

--- src/main/java/ca/ulaval/glo4003/offenses/domain/exceptions/InvalidOffenseCodeException.java
+++ src/main/java/ca/ulaval/glo4003/offenses/domain/exceptions/InvalidOffenseCodeException.java
@@ -1,10 +1,14 @@
 package ca.ulaval.glo4003.offenses.domain.exceptions;
 
-public class InvalidOffenseCodeException extends OffenseException {
+import ca.ulaval.glo4003.errors.domain.ErrorCode;
+import ca.ulaval.glo4003.errors.domain.exceptions.ApplicationException;
+
+public class InvalidOffenseCodeException extends ApplicationException {
   private static final String ERROR = "Invalid offense code";
   private static final String DESCRIPTION = "Offense code is invalid";
+  private static final ErrorCode CODE = ErrorCode.INVALID_REQUEST;
 
   public InvalidOffenseCodeException() {
-    super(ERROR, DESCRIPTION);
+    super(ERROR, DESCRIPTION, CODE);
   }
 }
