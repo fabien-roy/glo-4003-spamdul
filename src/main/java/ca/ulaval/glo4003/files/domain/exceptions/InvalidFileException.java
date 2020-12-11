6987e05a06a289d39fa2134112dcb19af226d518
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 18:35:27 -0500

    [ENHANCEMENT] Refactor files exceptions

--- src/main/java/ca/ulaval/glo4003/files/domain/exceptions/InvalidFileException.java
+++ src/main/java/ca/ulaval/glo4003/files/domain/exceptions/InvalidFileException.java
@@ -1,10 +1,14 @@
 package ca.ulaval.glo4003.files.domain.exceptions;
 
-public class InvalidFileException extends FileException {
+import ca.ulaval.glo4003.errors.domain.ErrorCode;
+import ca.ulaval.glo4003.errors.domain.exceptions.ApplicationException;
+
+public class InvalidFileException extends ApplicationException {
   private static final String ERROR = "Invalid File";
   private static final String DESCRIPTION = "File is invalid";
+  private static final ErrorCode CODE = ErrorCode.INVALID_REQUEST;
 
   public InvalidFileException() {
-    super(ERROR, DESCRIPTION);
+    super(ERROR, DESCRIPTION, CODE);
   }
 }
