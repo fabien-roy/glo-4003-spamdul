2dd71addc53ee0bcca0724410781da4ec6a9bb16
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 18:37:16 -0500

    [ENHANCEMENT] Refactor funds exceptions

--- src/main/java/ca/ulaval/glo4003/funds/domain/exceptions/NegativeMoneyException.java
+++ src/main/java/ca/ulaval/glo4003/funds/domain/exceptions/NegativeMoneyException.java
@@ -1,10 +1,14 @@
 package ca.ulaval.glo4003.funds.domain.exceptions;
 
-public class NegativeMoneyException extends FundException {
+import ca.ulaval.glo4003.errors.domain.ErrorCode;
+import ca.ulaval.glo4003.errors.domain.exceptions.ApplicationException;
+
+public class NegativeMoneyException extends ApplicationException {
   private static final String ERROR = "Negative amount of money";
   private static final String DESCRIPTION = "Amount of money cannot be negative";
+  private static final ErrorCode CODE = ErrorCode.INVALID_REQUEST;
 
   public NegativeMoneyException() {
-    super(ERROR, DESCRIPTION);
+    super(ERROR, DESCRIPTION, CODE);
   }
 }
