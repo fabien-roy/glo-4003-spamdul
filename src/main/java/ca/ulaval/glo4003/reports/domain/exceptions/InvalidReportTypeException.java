cf4c7a30cb3155c613fe3f73718a357a21a4a25a
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 18:43:34 -0500

    [ENHANCEMENT] Refactor reports exceptions

--- src/main/java/ca/ulaval/glo4003/reports/domain/exceptions/InvalidReportTypeException.java
+++ src/main/java/ca/ulaval/glo4003/reports/domain/exceptions/InvalidReportTypeException.java
@@ -1,10 +1,14 @@
 package ca.ulaval.glo4003.reports.domain.exceptions;
 
-public class InvalidReportTypeException extends ReportException {
+import ca.ulaval.glo4003.errors.domain.ErrorCode;
+import ca.ulaval.glo4003.errors.domain.exceptions.ApplicationException;
+
+public class InvalidReportTypeException extends ApplicationException {
   private static final String ERROR = "Invalid report type";
   private static final String DESCRIPTION = "Report type must be monthly, summary or dayOfMonth";
+  private static final ErrorCode CODE = ErrorCode.INVALID_REQUEST;
 
   public InvalidReportTypeException() {
-    super(ERROR, DESCRIPTION);
+    super(ERROR, DESCRIPTION, CODE);
   }
 }
