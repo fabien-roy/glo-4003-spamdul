8a693e913b3f378cf4934913cc570340ceb03f07
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 18:45:23 -0500

    [ENHANCEMENT] Refactor schedulers exceptions

--- src/main/java/ca/ulaval/glo4003/files/domain/exceptions/InvalidFileException.java
+++ src/main/java/ca/ulaval/glo4003/files/domain/exceptions/InvalidFileException.java
@@ -3,10 +3,11 @@ package ca.ulaval.glo4003.files.domain.exceptions;
 import ca.ulaval.glo4003.errors.domain.ErrorCode;
 import ca.ulaval.glo4003.errors.domain.exceptions.ApplicationException;
 
+// TODO #305 : Give file name
 public class InvalidFileException extends ApplicationException {
-  private static final String ERROR = "Invalid File";
+  private static final String ERROR = "Invalid file";
   private static final String DESCRIPTION = "File is invalid";
-  private static final ErrorCode CODE = ErrorCode.INVALID_REQUEST;
+  private static final ErrorCode CODE = ErrorCode.APPLICATION_FAILURE;
 
   public InvalidFileException() {
     super(ERROR, DESCRIPTION, CODE);
