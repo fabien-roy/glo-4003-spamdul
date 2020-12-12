9d713b17c1ba1b7e9d6e1ed5cf3064de58f85a81
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 20:11:43 -0500

    [FEATURE] List valid days of week

--- src/main/java/ca/ulaval/glo4003/times/domain/exceptions/InvalidDayOfWeekException.java
+++ src/main/java/ca/ulaval/glo4003/times/domain/exceptions/InvalidDayOfWeekException.java
@@ -1,10 +1,20 @@
 package ca.ulaval.glo4003.times.domain.exceptions;
 
-public class InvalidDayOfWeekException extends TimeException {
+import ca.ulaval.glo4003.errors.domain.ErrorCode;
+import ca.ulaval.glo4003.errors.domain.exceptions.ApplicationException;
+import ca.ulaval.glo4003.times.domain.DayOfWeek;
+
+public class InvalidDayOfWeekException extends ApplicationException {
   private static final String ERROR = "Invalid day of week";
-  private static final String DESCRIPTION = "Day of week is invalid";
+  private static final String DESCRIPTION = "Day of week should be one of %s";
+  private static final ErrorCode CODE = ErrorCode.INVALID_REQUEST;
 
   public InvalidDayOfWeekException() {
-    super(ERROR, DESCRIPTION);
+    super(ERROR, DESCRIPTION, CODE);
+  }
+
+  @Override
+  public String getDescription() {
+    return String.format(DESCRIPTION, enumerateValues(DayOfWeek.class));
   }
 }
