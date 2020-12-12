35a7698f0cb990637e0f48583aed9d0d61d951ae
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 20:28:26 -0500

    [FEATURE] Write postal code format

--- src/main/java/ca/ulaval/glo4003/locations/domain/exceptions/InvalidPostalCodeException.java
+++ src/main/java/ca/ulaval/glo4003/locations/domain/exceptions/InvalidPostalCodeException.java
@@ -1,10 +1,21 @@
 package ca.ulaval.glo4003.locations.domain.exceptions;
 
-public class InvalidPostalCodeException extends LocationException {
-  public static final String ERROR = "Invalid postal code";
-  public static final String DESCRIPTION = "Postal code is invalid";
+import ca.ulaval.glo4003.errors.domain.ErrorCode;
+import ca.ulaval.glo4003.errors.domain.exceptions.ApplicationException;
 
-  public InvalidPostalCodeException() {
-    super(ERROR, DESCRIPTION);
+public class InvalidPostalCodeException extends ApplicationException {
+  private static final String ERROR = "Invalid postal code";
+  private static final String DESCRIPTION = "Postal code must be of format : %s";
+  private static final ErrorCode CODE = ErrorCode.INVALID_REQUEST;
+  private final String format;
+
+  public InvalidPostalCodeException(String format) {
+    super(ERROR, DESCRIPTION, CODE);
+    this.format = format;
+  }
+
+  @Override
+  public String getDescription() {
+    return String.format(DESCRIPTION, format);
   }
 }
--- src/main/java/ca/ulaval/glo4003/locations/services/converters/PostalCodeConverter.java
+++ src/main/java/ca/ulaval/glo4003/locations/services/converters/PostalCodeConverter.java
@@ -6,10 +6,11 @@ import java.util.regex.Pattern;
 
 public class PostalCodeConverter {
   private static final Pattern PATTERN = Pattern.compile("[A-Z][0-9][A-Z] [0-9][A-Z][0-9]");
+  private static final String FORMAT = "XAX AXA, where X is an letter and A is a number";
 
   public PostalCode convert(String postalCode) {
     if (!PATTERN.matcher(postalCode).matches()) {
-      throw new InvalidPostalCodeException();
+      throw new InvalidPostalCodeException(FORMAT);
     }
 
     return new PostalCode(postalCode);
