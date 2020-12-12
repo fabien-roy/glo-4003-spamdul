3a6f1affca46175e4ab882bc7cbad0ee9de51f98
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 20:34:06 -0500

    [FEATURE] Write date time format

--- src/main/java/ca/ulaval/glo4003/times/domain/exceptions/InvalidDateTimeException.java
+++ src/main/java/ca/ulaval/glo4003/times/domain/exceptions/InvalidDateTimeException.java
@@ -1,10 +1,21 @@
 package ca.ulaval.glo4003.times.domain.exceptions;
 
-public class InvalidDateTimeException extends TimeException {
-  private static final String ERROR = "Invalid date time";
-  private static final String DESCRIPTION = "Date time must be of format 'dd-MM-yyyy HH:mm:ss'";
+import ca.ulaval.glo4003.errors.domain.ErrorCode;
+import ca.ulaval.glo4003.errors.domain.exceptions.ApplicationException;
 
-  public InvalidDateTimeException() {
-    super(ERROR, DESCRIPTION);
+public class InvalidDateTimeException extends ApplicationException {
+  private static final String ERROR = "Invalid date time";
+  private static final String DESCRIPTION = "Date time must be of format : %s";
+  private static final ErrorCode CODE = ErrorCode.INVALID_REQUEST;
+  private final String format;
+
+  public InvalidDateTimeException(String format) {
+    super(ERROR, DESCRIPTION, CODE);
+    this.format = format;
+  }
+
+  @Override
+  public String getDescription() {
+    return String.format(DESCRIPTION, format);
   }
 }
