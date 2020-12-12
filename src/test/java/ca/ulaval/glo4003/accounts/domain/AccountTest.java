199524b46ddf977f7003f59ab9d1db32abc999fc
Fabien Roy <fabienroy28@gmail.com>
2020-12-12 12:12:55 -0500

    [FIX] Fix style

--- src/test/java/ca/ulaval/glo4003/accounts/domain/AccountTest.java
+++ src/test/java/ca/ulaval/glo4003/accounts/domain/AccountTest.java
@@ -1,13 +1,12 @@
 package ca.ulaval.glo4003.accounts.domain;
 
-import ca.ulaval.glo4003.cars.domain.Car;
-import ca.ulaval.glo4003.cars.domain.exceptions.AlreadyExistingCarException;
-import org.junit.Test;
-
-import java.util.Collections;
-
 import static ca.ulaval.glo4003.accounts.helpers.AccountBuilder.anAccount;
 import static ca.ulaval.glo4003.cars.helpers.CarBuilder.aCar;
+
+import ca.ulaval.glo4003.cars.domain.Car;
+import ca.ulaval.glo4003.cars.domain.exceptions.AlreadyExistingCarException;
+import java.util.Collections;
+import org.junit.Test;
 
 public class AccountTest {
   @Test(expected = AlreadyExistingCarException.class)
