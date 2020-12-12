aa7627ba47bef9d6af332ebbbcf1919e7307bd22
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 20:13:01 -0500

    [FEATURE] List valid sex

--- src/main/java/ca/ulaval/glo4003/users/domain/exceptions/InvalidSexException.java
+++ src/main/java/ca/ulaval/glo4003/users/domain/exceptions/InvalidSexException.java
@@ -1,10 +1,20 @@
 package ca.ulaval.glo4003.users.domain.exceptions;
 
-public class InvalidSexException extends UserException {
+import ca.ulaval.glo4003.errors.domain.ErrorCode;
+import ca.ulaval.glo4003.errors.domain.exceptions.ApplicationException;
+import ca.ulaval.glo4003.users.domain.Sex;
+
+public class InvalidSexException extends ApplicationException {
   private static final String ERROR = "Invalid sex";
-  private static final String DESCRIPTION = "Sex should be m, f or x";
+  private static final String DESCRIPTION = "Sex should be one of %s";
+  private static final ErrorCode CODE = ErrorCode.INVALID_REQUEST;
 
   public InvalidSexException() {
-    super(ERROR, DESCRIPTION);
+    super(ERROR, DESCRIPTION, CODE);
+  }
+
+  @Override
+  public String getDescription() {
+    return String.format(DESCRIPTION, enumerateValues(Sex.class));
   }
 }
