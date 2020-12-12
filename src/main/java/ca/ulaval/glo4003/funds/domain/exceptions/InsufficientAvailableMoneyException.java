bc0729fa5f605ca95375ed418e7368b001b0c17c
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 21:54:17 -0500

    [FIX] Add error code to new exception

--- src/main/java/ca/ulaval/glo4003/funds/domain/exceptions/InsufficientAvailableMoneyException.java
+++ src/main/java/ca/ulaval/glo4003/funds/domain/exceptions/InsufficientAvailableMoneyException.java
@@ -1,12 +1,15 @@
 package ca.ulaval.glo4003.funds.domain.exceptions;
 
-// TODO : Implement ApplicationException
-public class InsufficientAvailableMoneyException extends FundException {
+import ca.ulaval.glo4003.errors.domain.ErrorCode;
+import ca.ulaval.glo4003.errors.domain.exceptions.ApplicationException;
+
+public class InsufficientAvailableMoneyException extends ApplicationException {
   private static final String ERROR = "Insufficient available money";
   private static final String DESCRIPTION =
       "Not enough available money to remove the requested amount";
+  private static final ErrorCode CODE = ErrorCode.INVALID_REQUEST;
 
   public InsufficientAvailableMoneyException() {
-    super(ERROR, DESCRIPTION);
+    super(ERROR, DESCRIPTION, CODE);
   }
 }
