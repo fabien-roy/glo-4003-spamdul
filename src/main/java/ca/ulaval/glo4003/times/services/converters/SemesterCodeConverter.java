6e630cac1cbbbf5f292d82a27f3d384f1e81937e
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 20:37:04 -0500

    [FEATURE] Write semester code format

--- src/main/java/ca/ulaval/glo4003/times/services/converters/SemesterCodeConverter.java
+++ src/main/java/ca/ulaval/glo4003/times/services/converters/SemesterCodeConverter.java
@@ -5,15 +5,12 @@ import ca.ulaval.glo4003.times.domain.exceptions.InvalidSemesterCodeException;
 import java.util.regex.Pattern;
 
 public class SemesterCodeConverter {
+  private static final String FORMAT = "XNN, where X is A, E or H and N is a number";
   private static final Pattern PATTERN = Pattern.compile("[AEH]\\d\\d");
 
   public SemesterCode convert(String code) {
-    if (code == null) {
-      throw new InvalidSemesterCodeException();
-    } else if (code.length() != 3) {
-      throw new InvalidSemesterCodeException();
-    } else if (!PATTERN.matcher(code).matches()) {
-      throw new InvalidSemesterCodeException();
+    if (code == null || !PATTERN.matcher(code).matches()) {
+      throw new InvalidSemesterCodeException(FORMAT);
     }
 
     return new SemesterCode(code);
