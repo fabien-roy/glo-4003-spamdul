d634366f99ca6b5ed059a77414f4d98b4246bd44
Fabien Roy <fabienroy28@gmail.com>
2020-12-11 19:52:20 -0500

    [FEATURE] List valid monthly payment status

--- src/main/java/ca/ulaval/glo4003/carboncredits/domain/exceptions/InvalidMonthlyPaymentStatusException.java
+++ src/main/java/ca/ulaval/glo4003/carboncredits/domain/exceptions/InvalidMonthlyPaymentStatusException.java
@@ -1,10 +1,20 @@
 package ca.ulaval.glo4003.carboncredits.domain.exceptions;
 
-public class InvalidMonthlyPaymentStatusException extends CarbonCreditException {
+import ca.ulaval.glo4003.carboncredits.domain.MonthlyPaymentStatus;
+import ca.ulaval.glo4003.errors.domain.ErrorCode;
+import ca.ulaval.glo4003.errors.domain.exceptions.ApplicationException;
+
+public class InvalidMonthlyPaymentStatusException extends ApplicationException {
   private static final String ERROR = "Invalid monthly payment status";
-  private static final String DESCRIPTION = "Montly payment status is not valid";
+  private static final String DESCRIPTION = "Monthly payment status should be one of %s";
+  private static final ErrorCode CODE = ErrorCode.INVALID_REQUEST;
 
   public InvalidMonthlyPaymentStatusException() {
-    super(ERROR, DESCRIPTION);
+    super(ERROR, DESCRIPTION, CODE);
+  }
+
+  @Override
+  public String getDescription() {
+    return String.format(DESCRIPTION, enumerateValues(MonthlyPaymentStatus.class));
   }
 }
