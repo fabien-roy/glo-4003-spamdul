a120ee40958d2c4e4bd262969d15a062aa07bb33
Fabien Roy <fabienroy28@gmail.com>
2020-12-12 19:21:17 -0500

    [FIX] Add more missing changes

--- src/main/java/ca/ulaval/glo4003/accesspasses/services/converters/AccessPassConverter.java
+++ src/main/java/ca/ulaval/glo4003/accesspasses/services/converters/AccessPassConverter.java
@@ -14,6 +14,7 @@ import ca.ulaval.glo4003.communications.services.converters.EmailAddressConverte
 import ca.ulaval.glo4003.locations.domain.PostalCode;
 import ca.ulaval.glo4003.locations.services.converters.PostalCodeConverter;
 import ca.ulaval.glo4003.parkings.domain.ParkingAreaCode;
+import ca.ulaval.glo4003.parkings.domain.ParkingConfiguration;
 import ca.ulaval.glo4003.parkings.domain.ReceptionMethod;
 import ca.ulaval.glo4003.parkings.services.assemblers.ParkingAreaCodeAssembler;
 import ca.ulaval.glo4003.times.domain.DayOfWeek;
@@ -59,7 +60,8 @@ public class AccessPassConverter {
     String[] scholarYear =
         SemesterCode.findScholarYearFromSemesterCode(
             new SemesterCodeConverter().convert(bicycleAccessPassDto.semester));
-    ParkingAreaCode parkingAreaCode = new ParkingAreaCode("ZoneVelo");
+    ParkingAreaCode parkingAreaCode =
+        ParkingConfiguration.getConfiguration().getBicycleParkingAreaCode();
     AccessPeriod accessPeriod = AccessPeriod.THREE_SEMESTERS;
 
     validateReceptionMethodForBicycleAccessPass(bicycleAccessPassDto);
