5996b2df5ca65c5336bb6bcad60be004e07ee4c0
Fabien Roy <fabienroy28@gmail.com>
2020-12-12 22:01:58 -0500

    [FIX] Fix flaky test

--- src/test/java/ca/ulaval/glo4003/times/services/SemesterServiceTest.java
+++ src/test/java/ca/ulaval/glo4003/times/services/SemesterServiceTest.java
@@ -1,6 +1,6 @@
 package ca.ulaval.glo4003.times.services;
 
-import static ca.ulaval.glo4003.times.helpers.SemesterCodeMother.createSemesterCode;
+import static ca.ulaval.glo4003.times.helpers.TimePeriodBuilder.aTimePeriod;
 import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -17,36 +17,35 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SemesterServiceTest {
-  private final String semester = createSemesterCode().toString();
-  private final String otherSemester = createSemesterCode().toString();
-
   @Mock private SemesterRepository semesterRepository;
   @Mock private SemesterCodeConverter semesterCodeConverter;
-  @Mock private SemesterCode semesterCode;
-  @Mock private SemesterCode otherSemesterCode;
-  @Mock private TimePeriod timePeriod;
-  @Mock private TimePeriod otherTimePeriod;
 
   private SemesterService semesterService;
 
+  private final SemesterCode semesterCode = new SemesterCode("A20");
+  private final SemesterCode otherSemesterCode = new SemesterCode("H20");
+  private final TimePeriod timePeriod = aTimePeriod().build();
+  private final TimePeriod otherTimePeriod = aTimePeriod().build();
+
   @Before
   public void setUp() {
-    when(semesterCodeConverter.convert(semester)).thenReturn(semesterCode);
-    when(semesterRepository.findByCode(semesterCode)).thenReturn(timePeriod);
-    when(semesterCodeConverter.convert(otherSemester)).thenReturn(otherSemesterCode);
-    when(semesterRepository.findByCode(otherSemesterCode)).thenReturn(otherTimePeriod);
-
     semesterService = new SemesterService(semesterRepository, semesterCodeConverter);
+
+    when(semesterCodeConverter.convert(semesterCode.toString())).thenReturn(semesterCode);
+    when(semesterCodeConverter.convert(otherSemesterCode.toString())).thenReturn(otherSemesterCode);
+    when(semesterRepository.findByCode(semesterCode)).thenReturn(timePeriod);
+    when(semesterRepository.findByCode(otherSemesterCode)).thenReturn(otherTimePeriod);
   }
 
   @Test
   public void whenGettingSemester_thenReturnGoodTimePeriod() {
     List<TimePeriod> timePeriods =
-        semesterService.getSemester(new String[] {semester, otherSemester});
+        semesterService.getSemester(
+            new String[] {semesterCode.toString(), otherSemesterCode.toString()});
 
+    assertThat(timePeriods).hasSize(2);
     assertThat(timePeriods).contains(timePeriod);
     assertThat(timePeriods).contains(otherTimePeriod);
-    assertThat(timePeriods.size()).isEqualTo(2);
   }
 
   @Test
